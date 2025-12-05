78c6df713eb02628d7174be917ee92e427081a11
suriya-a9 <uriyaprakash@gmail.com>
2025-12-05 16:04:55 +0530

    Changes

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const config = require("./config/default");
+const logger = require("./logger");
+const connectDb = require("./config/db");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const serviceRouter = require("./modules/service/service.routes");
+const adminRouter = require("./modules/admin/admin.routes");
+const loadingRouter = require("./modules/user/loadingScreen/loading.routes");
+const introRouter = require("./modules/user/introScreen/intro.routes");
+const accountTypeRouter = require("./modules/user/accountType/account.routes");
+const roadRouter = require("./modules/user/road/road.routes");
+const blockRouter = require("./modules/user/block/block.routes");
+const userAccountRouter = require("./modules/userAccount/userAccount.routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 1000,
+  message: "Request limit reached. Please try again later.",
+});
+
+app.use(limiter);
+
+connectDb();
+
+app.use("/api/admin", adminRouter);
+app.use("/api/user/loading", loadingRouter);
+app.use("/api/service", serviceRouter);
+app.use("/api/intro", introRouter);
+app.use("/api/account-type", accountTypeRouter);
+app.use("/api/road", roadRouter);
+app.use("/api/block", blockRouter);
+app.use("/api/user-account", userAccountRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message || "internal server error",
+  });
+});
+
+const port = config.port || 5000;
+app.listen(port, () => logger.info(`Server running at ${port}`));
