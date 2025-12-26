8a8a04b3ebc508aaa6ef7334afca99018369cb8d
suriya-a9 <uriyaprakash@gmail.com>
2025-12-26 16:38:24 +0530

    After completed password reset with email sending and reset in browser with resend link on token expiry

--- backend/config/default.js
+++ backend/config/default.js
@@ -4,6 +4,9 @@ const config = {
     port: process.env.PORT,
     url: process.env.URL,
     node_env: process.env.NODE_ENV,
-    jwt: process.env.JWT_SECRET
+    jwt: process.env.JWT_SECRET,
+    mail_user: process.env.EMAIL_USER,
+    mail_pass: process.env.EMAIL_PASS,
+    frontend: process.env.FRONTEND_URL,
 }
 module.exports = config;
--- backend/modules/admin/admin.routes.js
+++ backend/modules/admin/admin.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { adminRegister, adminLogin, listAdmins, updateAdmin, resetPassword } = require('./admin.controller');
+const { adminRegister, adminLogin, listAdmins, updateAdmin, forgotPassword, resetPassword } = require('./admin.controller');
 
 const router = express.Router();
 
 router.post("/register", adminRegister);
 router.post('/login', adminLogin);
 router.get("/list", listAdmins);
-router.post("/forgot-password", resetPassword);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 router.post("/:id", updateAdmin);
 
 module.exports = router;
